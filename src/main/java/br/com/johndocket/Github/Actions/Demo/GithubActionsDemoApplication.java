fb423df3363cd514bcf38b39a892c6029490dcef
John Quispe <john.quispe@docket.com.br>
2023-07-11 14:01:05 -0300

    second commit

---
+++ src/main/java/br/com/johndocket/Github/Actions/Demo/GithubActionsDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.johndocket.Github.Actions.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/johndocket/Github/Actions/Demo/GithubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.johndocket.Github.Actions.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
